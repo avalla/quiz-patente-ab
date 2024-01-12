d4c55cd85ea37856fb030c81914fa667d0107b9e
Andrea Valla <valla.andrea@gmail.com>
2024-01-12 21:38:07 +0100

    feat: add netlify function

--- netlify/functions/quiz.mjs
+++ netlify/functions/quiz.mjs
@@ -2,5 +2,5 @@ import pickDomanda from '../../src/services/pick-domanda.js';
 
 export default async (req, context) => {
     const domanda = pickDomanda(null);
-    return new Response(domanda);
+    return new Response(JSON.stringify(domanda));
 };
