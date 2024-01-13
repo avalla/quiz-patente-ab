0173d83c71e4c45a22c8e3816093f5803746c586
Andrea Valla <valla.andrea@gmail.com>
2024-01-13 11:07:49 +0100

    feat: add netlify function

--- netlify/functions/quiz.mjs
+++ netlify/functions/quiz.mjs
@@ -2,5 +2,6 @@ import pickDomanda from '../../src/services/pick-domanda.js';
 
 export default async (req, context) => {
     const domanda = pickDomanda(null);
+    domanda.image = `https://quiz-patente.netlify.app/${domanda.image}.gif`
     return new Response(JSON.stringify(domanda));
 };
